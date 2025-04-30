e91a4479f2850f863945208c99c064c7d0744984
leecherwei <leecw-wp22@student.tarc.edu.my>
2025-04-30 20:50:49 +0800

    Calendar and schedule

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,77 @@
 import 'package:flutter/material.dart';
+import 'schedule_page.dart';
+import 'calendar_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyScheduleApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class MyScheduleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Habitect2',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-
-  final String title;
-
+class HomeScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 1;             // Default on Calendar
 
-  void _incrementCounter() {
+  final List<Widget> _pages = [
+    SchedulePage(),
+    CalendarScreen(),
+    Placeholder(), // For Streak (you can create separate page later)
+    Placeholder(), // For Account
+  ];
+
+  void _onItemTapped(int index) {
     setState(() {
-
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
+      body: _pages[_selectedIndex],
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orange.shade100,
+        onPressed: () {},
+        child: Icon(Icons.add, color: Colors.black),
       ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 8.0,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                  icon: Icon(Icons.home, color: _selectedIndex == 0 ? Colors.orange : Colors.grey),
+                  onPressed: () => _onItemTapped(0)),
+              IconButton(
+                  icon: Icon(Icons.calendar_today, color: _selectedIndex == 1 ? Colors.orange : Colors.grey),
+                  onPressed: () => _onItemTapped(1)),
+              SizedBox(width: 40), // space for FAB
+              IconButton(
+                  icon: Icon(Icons.show_chart, color: _selectedIndex == 2 ? Colors.orange : Colors.grey),
+                  onPressed: () => _onItemTapped(2)),
+              IconButton(
+                  icon: Icon(Icons.person, color: _selectedIndex == 3 ? Colors.orange : Colors.grey),
+                  onPressed: () => _onItemTapped(3)),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
