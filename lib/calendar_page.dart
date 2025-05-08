2ca4417ce6224b363b1c7fa9a71f140e8c6179ed
leecherwei <leecw-wp22@student.tarc.edu.my>
2025-05-08 13:07:57 +0800

    Calendar and schedule

--- lib/calendar_page.dart
+++ lib/calendar_page.dart
@@ -148,7 +148,9 @@ class _CalendarScreenState extends State<CalendarScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar:
+
+      AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
         iconTheme: const IconThemeData(color: Colors.black),
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'calendar_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+//import 'app.dart';
+//import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  await Firebase.initializeApp(); // Initialize Firebase
+  runApp( MyApp());
 }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
