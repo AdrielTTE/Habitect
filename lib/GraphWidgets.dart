25977fbbe36a6f9331d883c01e691fa0cd852fca
AdrielTTE <adrieltangte@gmail.com>
2025-05-11 01:08:51 +0800

    Finished streakTracking module

--- lib/GraphWidgets.dart
+++ lib/GraphWidgets.dart
@@ -1,35 +1,98 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:pie_chart/pie_chart.dart';
 import 'package:fl_heatmap/fl_heatmap.dart';
-
+import 'package:d_chart/d_chart.dart';
 
 class GraphWidgets {
   final rows = [
+    '23',
     '22',
+    '21',
     '20',
+    '19',
     '18',
+    '17',
     '16',
+    '15',
     '14',
+    '13',
     '12',
+    '11',
     '10',
+    '9',
     '8',
+    '7',
     '6',
+    '5',
     '4',
+    '3',
     '2',
+    '1',
     '0',
-
   ];
 
   final columns = [
-    '0', '10', '20', '30', '40', '50', '60'
-
+    '0',
+    '5',
+    '10',
+    '15',
+    '20',
+    '25',
+    '30',
+    '35',
+    '40',
+    '45',
+    '50',
+    '55',
   ];
 
-  final r = Random();
+  List<NumericData> test = [
+    NumericData(domain: 5, measure: 5),
+    NumericData(domain: 6, measure: 7),
+    NumericData(domain: 1, measure: 1),
+    NumericData(domain: 7, measure: 3),
+    NumericData(domain: 3, measure: 4),
+    NumericData(domain: 2, measure: 2),
+  ];
 
-  Widget buildGraphSection(String selectedCategory, int notDoneCount, int doneCount, int goalCount, int ongoingCount, int dailyCount, int monthlyCount, int weeklyCount, int customCount, int dailyCat, int familyCat, int worksCat, int schoolsCat, int groceriesCat, int exerciseCat, int othersCat) {
+  List<NumericData> getTaskTime(List<int> taskHour, List<int> taskMinute) {
+    List<NumericData> taskTime = []; // Initialize the list outside the loop
+
+    // Loop through the provided taskHour and taskMinute lists
+    for (int i = 0; i < taskHour.length; i++) {
+      // Create a NumericData object and add it to the taskTime list
+      taskTime.add(NumericData(domain: taskMinute[i], measure: taskHour[i]));
+    }
+    print('taskHour: $taskHour');
+    print('taskMinute: $taskMinute');
+    return taskTime; // Return the populated list after the loop
+  }
+
+  Widget buildGraphSection(
+    String selectedCategory,
+    int notDoneCount,
+    int doneCount,
+    int goalCount,
+    int ongoingCount,
+    int dailyCount,
+    int monthlyCount,
+    int weeklyCount,
+    int customCount,
+    int dailyCat,
+    int familyCat,
+    int worksCat,
+    int schoolsCat,
+    int groceriesCat,
+    int exerciseCat,
+    int othersCat,
+    List<int> taskHours,
+    List<int> taskMinutes,
+  ) {
+    List<NumericData> taskTime = getTaskTime(taskHours, taskMinutes);
+
+    print('taskHour: $taskHours');
+    print('Tasktime: $taskTime');
     if (selectedCategory == 'To Do') {
       // This section is for the "To Do" pie chart
       return Container(
@@ -93,7 +156,6 @@ class GraphWidgets {
             SizedBox(height: 20),
 
             Container(
-
               height: 350,
               decoration: BoxDecoration(
                 color: Colors.grey.shade200,
@@ -128,37 +190,49 @@ class GraphWidgets {
                   showChartValues: false,
                 ),
               ),
-
             ),
 
-            SizedBox(height:20),
+            SizedBox(height: 20),
             Container(
-              padding:EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade200,
-                  borderRadius: BorderRadius.circular(12),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withValues(alpha: 0.5),
-                      spreadRadius: 3,
-                      blurRadius: 7,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
-                ),
-              child: Heatmap(heatmapData: HeatmapData(rows: rows, columns: columns, items: [
-                for (int row = 0; row < rows.length; row++)
-                  for (int col = 0; col < columns.length; col++)
-                    HeatmapItem(
-                        value: r.nextDouble() * 6,
-                        xAxisLabel: columns[col],
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withValues(alpha: 0.5),
+                    spreadRadius: 3,
+                    blurRadius: 7,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Incompleted To Do Timings',
+                    style: TextStyle(fontSize: 24, color: Colors.black, fontWeight: FontWeight.bold)
+                  ),
+                  SizedBox(height: 10),
+                  AspectRatio(
+                    aspectRatio: 1 / 1,
+                    child: DChartScatterN(
+                      configSeriesScatter: ConfigSeriesScatterN(
+                        seriesColor: Colors.orange,
+                        showPointLabel: true,
+                        pointRadiusBase: 10,
 
-                        yAxisLabel: rows[row]),
-              ]),
+                        symbolRender: SymbolRenderCircle(isSolid: true),
+                      ),
+                      groupList: [NumericGroup(id: 'Task Time', data: taskTime)],
+                    ),
+                  ),
+                ],
               )
 
-              ),
 
+
+            ),
           ],
         ),
       );
@@ -309,8 +383,6 @@ class GraphWidgets {
                   showChartValues: false,
                 ),
               ),
-
-
             ),
           ],
         ),
