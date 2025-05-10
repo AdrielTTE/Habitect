471c050ab6ef65d6432f7883856a90fd4b63c53b
AdrielTTE <adrieltangte@gmail.com>
2025-05-10 09:25:48 +0800

    Modified streakTracking.dart

--- lib/streakTracking.dart
+++ lib/streakTracking.dart
@@ -297,84 +297,88 @@ class _StreakTrackingState extends State<StreakTracking> {
           dropdownColor: dropdownColor,
         ),
 
-        _buildDropdown<String>(
-          value: selectedYear,
-          items: years,
-          onChanged: (value) {
-            setState(() {
-              selectedYear = value!;
-              yearNumber = int.parse(selectedYear);
-              print(yearNumber);
-            });
-            // Fetch tasks based on the selected year
-            if (selectedCategory == 'To Do') {
-              _fetchToDoTasks();
-            } else {
-              _fetchGoalsData();
-            }
-          },
-          dropdownColor: dropdownColor,
-        ),
-        _buildDropdown<String>(
-          value: selectedMonth,
-          items: _getAvailableMonths(),
-          onChanged: (value) {
-
-            setState(() {
-              selectedMonth = value!;
-              switch (selectedMonth) {
-                case 'January':
-                  monthNumber = 1;
-                  break;
-                case 'February':
-                  monthNumber = 2;
-                  break;
-                case 'March':
-                  monthNumber = 3;
-                  break;
-                case 'April':
-                  monthNumber = 4;
-                  break;
-                case 'May':
-                  monthNumber = 5;
-                  break;
-                case 'June':
-                  monthNumber = 6;
-                  break;
-                case 'July':
-                  monthNumber = 7;
-                  break;
-                case 'August':
-                  monthNumber = 8;
-                  break;
-                case 'September':
-                  monthNumber = 9;
-                  break;
-                case 'October':
-                  monthNumber = 10;
-                  break;
-                case 'November':
-                  monthNumber = 11;
-                  break;
-                case 'December':
-                  monthNumber = 12;
-                  break;
-                default:
-                  monthNumber = -1;
-                  break;
+        if (selectedCategory == 'Goals')
+          _buildDropdown<String>(
+            value: selectedYear,
+            items: years,
+            onChanged: (value) {
+              setState(() {
+                selectedYear = value!;
+                yearNumber = int.parse(selectedYear);
+                print(yearNumber);
+              });
+              // Fetch tasks based on the selected year
+              if (selectedCategory == 'To Do') {
+                _fetchToDoTasks();
+              } else {
+                _fetchGoalsData();
               }
-              print(selectedMonth);
-              print(monthNumber);
-            });
-            // Fetch tasks again based on the selected month
-            if (selectedCategory == 'To Do') {
-              _fetchToDoTasks();
-            } else {
-              _fetchGoalsData();
-            }
-          },
-          dropdownColor: dropdownColor,
-        ),
+            },
+            dropdownColor: dropdownColor,
+          ),
+        if (selectedCategory == 'Goals')
+          _buildDropdown<String>(
+            value: selectedMonth,
+            items: _getAvailableMonths(),
+            onChanged: (value) {
+
+              setState(() {
+                selectedMonth = value!;
+                switch (selectedMonth) {
+                  case 'January':
+                    monthNumber = 1;
+                    break;
+                  case 'February':
+                    monthNumber = 2;
+                    break;
+                  case 'March':
+                    monthNumber = 3;
+                    break;
+                  case 'April':
+                    monthNumber = 4;
+                    break;
+                  case 'May':
+                    monthNumber = 5;
+                    break;
+                  case 'June':
+                    monthNumber = 6;
+                    break;
+                  case 'July':
+                    monthNumber = 7;
+                    break;
+                  case 'August':
+                    monthNumber = 8;
+                    break;
+                  case 'September':
+                    monthNumber = 9;
+                    break;
+                  case 'October':
+                    monthNumber = 10;
+                    break;
+                  case 'November':
+                    monthNumber = 11;
+                    break;
+                  case 'December':
+                    monthNumber = 12;
+                    break;
+                  default:
+                    monthNumber = -1;
+                    break;
+                }
+                print(selectedMonth);
+                print(monthNumber);
+              });
+              // Fetch tasks again based on the selected month
+              if (selectedCategory == 'To Do') {
+                _fetchToDoTasks();
+              } else {
+                _fetchGoalsData();
+              }
+            },
+            dropdownColor: dropdownColor,
+          ),
+
+
       ],
     );
   }
